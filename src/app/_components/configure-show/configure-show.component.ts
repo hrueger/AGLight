898227d64b92f890f485c5e33acaa1119134c092
Hannes Rüger <hannesrueger@gmx.de>
2019-10-21 20:13:11 -1000

    added slider, button grid, button widget

--- src/app/_components/configure-show/configure-show.component.ts
+++ src/app/_components/configure-show/configure-show.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { GridsterConfig, GridsterItem } from "angular-gridster2";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { GridsterConfig } from "angular-gridster2";
 import * as smalltalk from "smalltalk";
+import { colors } from "../../_ressources/colors";
+import { controls } from "../../_ressources/controls";
 import { ShowService } from "../../_services/show.service";
 import * as smalltalkSelect from "../../_utils/smalltalk-select";
 
@@ -11,28 +13,35 @@ import * as smalltalkSelect from "../../_utils/smalltalk-select";
 })
 export class ConfigureShowComponent implements OnInit {
   public options: GridsterConfig;
-  public ui: GridsterItem[];
+  public ui: Array<{
+    x: number,
+    y: number,
+    rows: number,
+    cols: number,
+    widget: string,
+    effectOrHead: string,
+    headIdx: number,
+    effectOrChannelIdx: number,
+    effectParamIdx?: number,
+  }>;
   public heads;
+  private readonly shadeColorFactor = 35;
+
   constructor(private showService: ShowService) {}
 
   public ngOnInit() {
+    const that = this;
     this.heads = this.showService.getData("usedHeads");
     this.ui = this.showService.getData("ui") || [];
     this.options = {
-      displayGrid: "always",
+      displayGrid: "onDrag&Resize",
       draggable: {enabled: true},
-      itemChangeCallback: this.onItemChanged,
+      itemChangeCallback: () => {
+        that.save();
+      },
+      mobileBreakpoint: 0,
       resizable: {enabled: true},
     };
-
-    this.ui = [
-      { cols: 2, rows: 1, y: 0, x: 0 },
-      { cols: 2, rows: 2, y: 0, x: 2 },
-      { cols: 1, rows: 1, y: 0, x: 4 },
-      { cols: 1, rows: 1, y: 2, x: 5 },
-      { cols: 1, rows: 1, y: 1, x: 0 },
-      { cols: 1, rows: 1, y: 1, x: 0 },
-    ];
   }
 
   public removeItem($event, item) {
@@ -54,8 +63,8 @@ export class ConfigureShowComponent implements OnInit {
           const headIdx = parseInt(i, undefined);
           options = [
             {
-              description: "Add a control from a head.",
-              name: "Head",
+              description: "Add a control from a channel.",
+              name: "Channel",
               value: "head",
             },
             {
@@ -65,7 +74,7 @@ export class ConfigureShowComponent implements OnInit {
             },
           ];
           smalltalkSelect.select("Add control",
-          "Choose the general type from which you want to add a control.", options, {}).then((effectOrHead) => {
+          "Choose the general type from which you want to add a control.", options, {}).then((effectOrHead: string) => {
           let msg;
           if (effectOrHead == "head") {
             msg = "Choose the channel:";
@@ -88,7 +97,7 @@ export class ConfigureShowComponent implements OnInit {
             });
           }
           if (options.length) {
-            smalltalkSelect.select("Add control", msg, options, {}).then((j: string) => {
+            smalltalkSelect.select("Add control", msg, options, {}).then(async (j: string) => {
               const effectOrChannelIdx = parseInt(j, undefined);
               let effectParamIdx;
               if (effectOrHead == "effect") {
@@ -99,28 +108,218 @@ export class ConfigureShowComponent implements OnInit {
                     value: index,
                   };
                 });
-                smalltalkSelect.select("Add control",
-                  "Choose the effect parameter to control", options, {}).then((k: string) => {
-                  effectParamIdx = parseInt(k, undefined);
-                }, () => undefined);
+                effectParamIdx = parseInt(await (smalltalkSelect.select("Add control",
+                  "Choose the effect parameter to control", options, {}) as Promise<string>), undefined);
               }
 
               const controlType = (effectOrHead == "head" ?
               this.heads[headIdx].channelMode.channels[effectOrChannelIdx].type :
               this.heads[headIdx].effects[effectOrChannelIdx].params[effectParamIdx].type);
-              console.log(controlType);
-              // ask for wanted control
+
+              options = controls.filter((control) => {
+                return control.type == controlType;
+              })[0].usefulWidgets.map((widget) => {
+                return {
+                  description: `Add a ${widget}.`,
+                  name: widget,
+                  value: widget,
+                };
+              });
+              if (options.length) {
+                smalltalkSelect.select("Add control",
+                  "Choose the control you want to add:", options, {}).then((widget: string) => {
+                  this.ui.push({
+                    cols: 1,
+                    effectOrChannelIdx,
+                    effectOrHead,
+                    effectParamIdx,
+                    headIdx,
+                    rows: 1,
+                    widget,
+                    x: 0,
+                    y: 0,
+                  });
+                  this.save();
+                }, () => undefined);
+              } else {
+                this.alertNothingToDisplay();
+              }
             }, () => undefined);
           } else {
-            smalltalk.alert("Error", "Unfortunately there is nothing to display... Please try other options!");
+            this.alertNothingToDisplay();
           }
         }, () => undefined);
 
-  }, () => undefined);
-    this.ui.push({ x: 0, y: 0, cols: 1, rows: 1 });
+   }, () => undefined);
   }
 
-  public onItemChanged(gridsterItem, gridsterItemComponent) {
-    console.log("changes: ", gridsterItem, gridsterItemComponent);
+  public save() {
+    this.showService.setData("ui", this.ui);
   }
+
+  public getButtongridRowArray(control) {
+    const nodes: [] = (control.effectOrHead == "head" ?
+    (this.heads[control.headIdx].channelMode.channels[control.effectOrChannelIdx].configNodes) :
+    ((this.heads[control.headIdx].effects[control.effectOrChannelIdx].params[control.effectParamIdx].configNodes)));
+    const rows = Math.floor(Math.sqrt(nodes.length));
+    // const cols = nodes.length / rows;
+    return Array.from(Array(rows).keys());
+  }
+
+  public getButtongridColArray(control, i) {
+    const nodes: [] = (control.effectOrHead == "head" ?
+    (this.heads[control.headIdx].channelMode.channels[control.effectOrChannelIdx].configNodes) :
+    ((this.heads[control.headIdx].effects[control.effectOrChannelIdx].params[control.effectParamIdx].configNodes)));
+    const rows = Math.floor(Math.sqrt(nodes.length));
+    const cols = nodes.length / rows;
+    return Array.from(Array(cols).keys()).map((n) => n + (i * rows) + i);
+  }
+
+  public tryGetBackgroundColor(name: string) {
+    name = name.toLowerCase().replace("-", " ");
+    const c = colors.filter((color) => color.name.toLowerCase() == name)[0];
+    if (c) { // color found in list
+      return {
+        backgroundColor: c.hex,
+        color: this.isColorLight(c.hex) ? "#000" : "#fff",
+      };
+    } else if (name.startsWith("rainbow")) { // check for special words
+      return {
+        background: "linear-gradient(to right, red, orange, yellow, green, cyan, blue, violet)",
+        textShadow: "2px 2px 2px #000",
+      };
+    } else {
+      const parts = name.split("+");
+      const clrs: string[] = [];
+      parts.forEach((part) => {
+        part = part.replace(/[^a-zA-Z0-9]/g, "");
+        const col = colors.filter((color) => 
+        color.name.toLowerCase() == part || color.name.toLowerCase().replace(" ", "") == part.replace(" ", ""))[0];
+        if (col) { // color found in list
+          clrs.push(col.hex);
+        } else {
+          const n = this.replaceDarkOrLight(part);
+          //console.log(n);
+          if (n) {
+            clrs.push(n.backgroundColor);
+          } else {
+            clrs.push("#757575");
+          }
+        }
+      });
+      if (clrs.length == 1) {
+        return {
+          backgroundColor: clrs[0],
+          color: this.isColorLight(clrs[0]) ? "#000" : "#fff",
+        };
+      } else {
+        return {
+          background: `linear-gradient(to right, ${clrs.join(", ")})`,
+          textShadow: "2px 2px 2px #000",
+        };
+      }
+
+      /*if (name.startsWith("light") || name.startsWith("dark")) {
+      const newColor = this.replaceDarkOrLight(name);
+      if (newColor) {
+        return newColor;
+      }
+    }*/
+    }
+    /* else {
+      return {
+        backgroundColor: "#757575",
+      };
+    }*/
+  }
+
+  private alertNothingToDisplay() {
+    smalltalk.alert("Error", "Unfortunately there is nothing to display... Please try other options!");
+  }
+
+  private replaceDarkOrLight(name) {
+    if (name.startsWith("dark")) {
+      name = name.replace("dark", "").replace(/[^a-zA-Z0-9]/g, "");
+      // console.log("dark, looking for", name);
+      // console.log(colors);
+      const c = colors.filter((color) => color.name.toLowerCase() == name)[0];
+      // console.log(c);
+      if (c) {
+        const hex = this.shadeColor(c.hex, -this.shadeColorFactor);
+        return {
+          backgroundColor: hex,
+          color: this.isColorLight(c.hex) ? "#000" : "#fff",
+        };
+      }
+    } else if (name.startsWith("light")) {
+      name = name.replace("light", "").replace(/[^a-zA-Z0-9]/g, "");
+      // console.log("light, looking for", name)
+      const c = colors.filter((color) => color.name.toLowerCase() == name)[0];
+      // console.log(c);
+      if (c) {
+        const hex = this.shadeColor(c.hex, this.shadeColorFactor);
+        return {
+          backgroundColor: hex,
+          color: this.isColorLight(c.hex) ? "#000" : "#fff",
+        };
+      }
+    }
+  }
+  private isColorLight(color) {
+    // tslint:disable-next-line: one-variable-per-declaration
+    let r, g, b, hsp;
+    if (color.match(/^rgb/)) {
+        color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+
+        r = color[1];
+        g = color[2];
+        b = color[3];
+    } else {
+        color = +("0x" + color.slice(1).replace(
+        color.length < 5 && /./g, "$&$&"));
+        // tslint:disable-next-line: no-bitwise
+        r = color >> 16;
+        // tslint:disable-next-line: no-bitwise
+        g = color >> 8 & 255;
+        // tslint:disable-next-line: no-bitwise
+        b = color & 255;
+    }
+
+    hsp = Math.sqrt(
+    0.299 * (r * r) +
+    0.587 * (g * g) +
+    0.114 * (b * b),
+    );
+    if (hsp > 127.5) {
+        return true;
+    } else {
+        return false;
+    }
+  }
+
+  private shadeColor(color, percent) {
+    let R = parseInt(color.substring(1, 3), 16);
+    let G = parseInt(color.substring(3, 5), 16);
+    let B = parseInt(color.substring(5, 7), 16);
+
+    // @ts-ignore
+    // tslint:disable-next-line: radix
+    R = parseInt(R * (100 + percent) / 100);
+    // @ts-ignore
+    // tslint:disable-next-line: radix
+    G = parseInt(G * (100 + percent) / 100);
+    // @ts-ignore
+    // tslint:disable-next-line: radix
+    B = parseInt(B * (100 + percent) / 100);
+
+    R = (R < 255) ? R : 255;
+    G = (G < 255) ? G : 255;
+    B = (B < 255) ? B : 255;
+
+    const RR = ((R.toString(16).length == 1) ? "0" + R.toString(16) : R.toString(16));
+    const GG = ((G.toString(16).length == 1) ? "0" + G.toString(16) : G.toString(16));
+    const BB = ((B.toString(16).length == 1) ? "0" + B.toString(16) : B.toString(16));
+
+    return "#" + RR + GG + BB;
+}
 }
