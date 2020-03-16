23006fe5c27f7f0269209945e562859bf82d8434
H. Rüger <hannesrueger@gmx.de>
2020-03-16 09:26:45 -1000

    added basic parsing

--- src/app/_services/library.service.ts
+++ src/app/_services/library.service.ts
@@ -8,6 +8,7 @@ import * as request from "request";
 import { StatusbarService } from "./statusbar.service";
 import { Product } from "../_entities/product";
 import { RawProduct } from "../_entities/rawProduct";
+import { colors } from "../_ressources/colors";
 
 @Injectable({
     providedIn: "root",
@@ -17,7 +18,7 @@ export class LibraryService {
     private libraryPath = path.join((electron.app || electron.remote.app).getPath("userData"), "library");
     private productCache: Product[] = [];
 
-    constructor(private statusbarService: StatusbarService) {}
+    constructor(private statusbarService: StatusbarService) { }
 
     public getProducts(): Product[] {
         return this.productCache;
@@ -40,10 +41,82 @@ export class LibraryService {
             this.sync();
         }
         const manufacturerCache = JSON.parse(fs.readFileSync(path.join(this.libraryPath, "manufacturers.json")).toString());
+        const units = ["K", "deg", "%", "ms", "Hz", "m^3/min"];
+        const excludeKeys = ["comment", "name", "helpWanted", "type"];
         this.walk(this.libraryPath, (err, files) => {
             files.forEach((file) => {
                 const content = JSON.parse(fs.readFileSync(file).toString()) as RawProduct;
                 content.manufacturer = manufacturerCache[content.manufacturerKey];
+                if (content.availableChannels) {
+                    for (const k of Object.keys(content.availableChannels)) {
+                        if (content.availableChannels[k].capability) {
+                            for (const k2 of Object.keys(content.availableChannels[k].capability)) {
+                                if ((typeof content.availableChannels[k].capability[k2] == "string") && (!excludeKeys.includes(k2))) {
+                                    for (const u of units) {
+                                        if ((typeof content.availableChannels[k].capability[k2] == "string") && content.availableChannels[k].capability[k2].endsWith(u)) {
+                                            content.availableChannels[k].capability[k2].replace(u, "");
+                                            content.availableChannels[k].capability[k2] = parseInt(content.availableChannels[k].capability[k2]);
+                                        }
+                                    }
+                                    if ((typeof content.availableChannels[k].capability[k2] == "string") && content.availableChannels[k].capability[k2].endsWith("s")) {
+                                        content.availableChannels[k].capability[k2].replace("s", "");
+                                        content.availableChannels[k].capability[k2] = parseInt(content.availableChannels[k].capability[k2]) * 1000;
+                                    } else  if (parseInt(content.availableChannels[k].capability[k2])) {
+                                        content.availableChannels[k].capability[k2] = parseInt(content.availableChannels[k].capability[k2]);
+                                    } else if (content.availableChannels[k].capability[k2] == "slow") {
+                                        content.availableChannels[k].capability[k2] = 1;
+                                    } else if (content.availableChannels[k].capability[k2] == "fast") {
+                                        content.availableChannels[k].capability[k2] = 0.1;
+                                    } else if (content.availableChannels[k].capability[k2] == "low") {
+                                        content.availableChannels[k].capability[k2] = 0.1;
+                                    } else if (content.availableChannels[k].capability[k2] == "high") {
+                                        content.availableChannels[k].capability[k2] = 1;
+                                    } else if (content.availableChannels[k].capability[k2] == "long") {
+                                        content.availableChannels[k].capability[k2] = 1;
+                                    } else if (content.availableChannels[k].capability[k2] == "short") {
+                                        content.availableChannels[k].capability[k2] = 0.1;
+                                    } else if (content.availableChannels[k].capability[k2] == "big") {
+                                        content.availableChannels[k].capability[k2] = 1;
+                                    } else if (content.availableChannels[k].capability[k2] == "small") {
+                                        content.availableChannels[k].capability[k2] = 0.1;
+                                    } else if (content.availableChannels[k].capability[k2] == "instant") {
+                                        content.availableChannels[k].capability[k2] = 0.01;
+                                    } else if (content.availableChannels[k].capability[k2] == "wide") {
+                                        content.availableChannels[k].capability[k2] = 1;
+                                    }  else if (content.availableChannels[k].capability[k2] == "narrow") {
+                                        content.availableChannels[k].capability[k2] = 0.1;
+                                    } else if (content.availableChannels[k].capability[k2] == "far") {
+                                        content.availableChannels[k].capability[k2] = 1;
+                                    }  else if (content.availableChannels[k].capability[k2] == "near") {
+                                        content.availableChannels[k].capability[k2] = 0.1;
+                                    }  else if (content.availableChannels[k].capability[k2] == "off") {
+                                        content.availableChannels[k].capability[k2] = false;
+                                    }  else if (content.availableChannels[k].capability[k2] == "on") {
+                                        content.availableChannels[k].capability[k2] = true;
+                                    }  else if (content.availableChannels[k].capability[k2] == "closed") {
+                                        content.availableChannels[k].capability[k2] = false;
+                                    }  else if (content.availableChannels[k].capability[k2] == "open") {
+                                        content.availableChannels[k].capability[k2] = true;
+                                    }  else if (content.availableChannels[k].capability[k2] == "out") {
+                                        content.availableChannels[k].capability[k2] = false;
+                                    }  else if (content.availableChannels[k].capability[k2] == "in") {
+                                        content.availableChannels[k].capability[k2] = true;
+                                    } else {
+                                        const color = colors.filter((c) => c.name == content.availableChannels[k].capability[k2]);
+                                        if (color && color[0]) {
+                                            content.availableChannels[k].capability[k2] = color[0].hex;
+                                        } else {
+                                            // ToDo some more here
+                                            // console.log(k2, content.availableChannels[k].capability[k2])
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    // ToDo
+                }
                 this.productCache.push(content as any);
             });
             this.statusbarService.setItem({
