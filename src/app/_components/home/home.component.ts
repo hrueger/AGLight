bce55bd795cb2107394a58a71193cfe2ff54aad9
Hannes Rüger <hannesrueger@gmx.de>
2020-07-11 14:54:47 +0200

    created heads widget now opens ofl site

--- src/app/_components/home/home.component.ts
+++ src/app/_components/home/home.component.ts
@@ -6,64 +6,69 @@ import { ShowService } from "../../_services/show.service";
 import { LibraryService } from "../../_services/library.service";
 import { DmxService } from "../../_services/dmx.service";
 import { ConsoleService } from "../../_services/console.service";
+import { shell } from "electron";
 
 @Component({
-  selector: "app-home",
-  styleUrls: ["./home.component.scss"],
-  templateUrl: "./home.component.html",
+    selector: "app-home",
+    styleUrls: ["./home.component.scss"],
+    templateUrl: "./home.component.html",
 })
 export class HomeComponent {
-  public recentShows: any[] = [];
-  public showRecentDropdown: boolean = false;
-  private readonly waitTime: number = 3000;
-  constructor(private showService: ShowService,
-              private router: Router,
-              private recentShowsService: RecentShowsService,
-              private libraryService: LibraryService,
-              private dmxService: DmxService,
-              private consoleService: ConsoleService,
-              ) {}
+    public recentShows: any[] = [];
+    public showRecentDropdown: boolean = false;
+    private readonly waitTime: number = 3000;
+    constructor(private showService: ShowService,
+        private router: Router,
+        private recentShowsService: RecentShowsService,
+        private libraryService: LibraryService,
+        private dmxService: DmxService,
+        private consoleService: ConsoleService,
+    ) { }
 
-  public async ngOnInit() {
-    setTimeout(() => {
-      ipcRenderer.send("ready");
-    }, this.waitTime);
-    this.recentShows = this.recentShowsService.get();
-    this.dmxService.init();
-    this.consoleService.init();
-    await this.libraryService.loadIntoCache();
-  }
-  public newShow() {
-    const path = remote.dialog.showSaveDialogSync(remote.getCurrentWindow(), {
-      buttonLabel: "Create",
-      filters: [
-        {
-          extensions: ["aglshow"],
-          name: "AGLight show file",
-        },
-      ],
-      message: "Create show file",
-      title: "Create show file",
-    });
-    this.showService.createShow(path);
-  }
-  public recentShow(i) {
-    const path = this.recentShows[i];
-    this.showService.loadShow(path);
-  }
-  public openShow() {
-    const path = remote.dialog.showOpenDialogSync(remote.getCurrentWindow(), {
-      buttonLabel: "Open",
-      filters: [
-        {
-          extensions: ["aglshow"],
-          name: "AGLight show file",
-        },
-      ],
-      message: "Open show file",
-      properties: ["openFile"],
-      title: "Open show file",
-    })[0];
-    this.showService.loadShow(path);
-  }
+    public async ngOnInit() {
+        setTimeout(() => {
+            ipcRenderer.send("ready");
+        }, this.waitTime);
+        this.recentShows = this.recentShowsService.get();
+        this.dmxService.init();
+        this.consoleService.init();
+        await this.libraryService.loadIntoCache();
+    }
+    public newShow() {
+        const path = remote.dialog.showSaveDialogSync(remote.getCurrentWindow(), {
+            buttonLabel: "Create",
+            filters: [
+                {
+                    extensions: ["aglshow"],
+                    name: "AGLight show file",
+                },
+            ],
+            message: "Create show file",
+            title: "Create show file",
+        });
+        this.showService.createShow(path);
+    }
+    public recentShow(i) {
+        const path = this.recentShows[i];
+        this.showService.loadShow(path);
+    }
+    public openShow() {
+        const path = remote.dialog.showOpenDialogSync(remote.getCurrentWindow(), {
+            buttonLabel: "Open",
+            filters: [
+                {
+                    extensions: ["aglshow"],
+                    name: "AGLight show file",
+                },
+            ],
+            message: "Open show file",
+            properties: ["openFile"],
+            title: "Open show file",
+        })[0];
+        this.showService.loadShow(path);
+    }
+
+    public openExternal(url: string) {
+        shell.openExternal(url);
+    }
 }
