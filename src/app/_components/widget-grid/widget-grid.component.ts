092f8e695315d0a0ed167171f4f9a8edf996cc0b
Hannes Rüger <hannesrueger@gmx.de>
2020-07-13 14:16:14 +0200

    fixed button widgets not working

--- src/app/_components/widget-grid/widget-grid.component.ts
+++ src/app/_components/widget-grid/widget-grid.component.ts
@@ -147,6 +147,8 @@ export class WidgetGridComponent implements OnInit {
             this.dmxService.updateMultiple(event, channels);
             break;
         case "button":
+            channels = this.findChannelAddresses(widget);
+            this.dmxService.updateMultiple(widget.config?.buttonValue ? widget.config.buttonValue : 0, channels);
             break;
         case "buttongrid":
             // chl = this.findChannelAddress(widget);
