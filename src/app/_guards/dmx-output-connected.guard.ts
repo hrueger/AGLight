f64b45a3455e7160e50d9536a81afa126bd67f27
Hannes Rüger <hannesrueger@gmx.de>
2020-07-16 16:32:35 +0200

    navigate after connection established

--- src/app/_guards/dmx-output-connected.guard.ts
+++ src/app/_guards/dmx-output-connected.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import {
-    ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot,
+    ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router,
 } from "@angular/router";
 import { DmxService } from "../_services/dmx.service";
 
@@ -8,7 +8,7 @@ import { DmxService } from "../_services/dmx.service";
     providedIn: "root",
 })
 export class DmxOutputConnectedGuard implements CanActivate {
-    constructor(private dmxService: DmxService) {}
+    constructor(private dmxService: DmxService, private router: Router) {}
     public canActivate(
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         next: ActivatedRouteSnapshot,
@@ -18,7 +18,9 @@ export class DmxOutputConnectedGuard implements CanActivate {
         if (this.dmxService.isConnected) {
             return true;
         }
-        this.dmxService.connectToDevice();
+        this.dmxService.connectToDevice(() => {
+            this.router.navigate(["show"]);
+        });
         return false;
     }
 }
