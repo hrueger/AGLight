1635bbb68960a47a2375cce076a550cc4607d2e2
Hannes Rüger <hannesrueger@gmx.de>
2020-07-16 16:22:20 +0200

    fixed linting errors

--- src/app/_utils/find-channel-addresses.ts
+++ src/app/_utils/find-channel-addresses.ts
@@ -5,7 +5,9 @@ export function findChannelAddresses(widget: Widget): number[] {
     return new Array(widget.fixture.number)
         .fill(null)
         .map((_, idx) => widget.fixture.startAddress
-            + ( getChannelCount(widget.fixture) == 1 ? idx : (idx * (getChannelCount(widget.fixture) + 1)))
+            + (getChannelCount(widget.fixture) == 1
+                ? idx
+                : (idx * (getChannelCount(widget.fixture) + 1)))
             + widget.fixture.product.modes
                 .filter((m) => m.name == widget.fixture.channelMode)[0]
                 .channels.findIndex((c) => c == widget.channel));
