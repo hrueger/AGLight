fe94691e9e5376dabd62d8308405fb710702fd79
Hannes Rüger <hannesrueger@gmx.de>
2020-07-18 18:23:48 +0200

    added confirm dialog when closing window

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Router, RouterEvent } from "@angular/router";
 import { remote } from "electron";
+import * as smalltalk from "smalltalk";
 import { ShowService } from "../../_services/show.service";
 
 @Component({
@@ -9,64 +10,67 @@ import { ShowService } from "../../_services/show.service";
     templateUrl: "./navbar.component.html",
 })
 export class NavbarComponent {
-  public isMaximized = true;
-  public showLoaded = false;
-  public showSettingsDropdown = false;
-  public canNavigate = true;
+    public isMaximized = true;
+    public showLoaded = false;
+    public showSettingsDropdown = false;
+    public canNavigate = true;
 
-  public menuItems: any[] = [
-      {
-          name: "Fixtures",
-          url: "fixtures",
-      },
-      {
-          seperator: true,
-      },
-      {
-          name: "Config",
-          url: "configureShow",
-      },
-      {
-          seperator: true,
-      },
-      {
-          name: "Show",
-          url: "show",
-      },
-  ];
-  private win: any;
-  constructor(public showService: ShowService, private router: Router) {}
+    public menuItems: any[] = [
+        {
+            name: "Fixtures",
+            url: "fixtures",
+        },
+        {
+            seperator: true,
+        },
+        {
+            name: "Config",
+            url: "configureShow",
+        },
+        {
+            seperator: true,
+        },
+        {
+            name: "Show",
+            url: "show",
+        },
+    ];
+    private win: any;
+    constructor(public showService: ShowService, private router: Router) { }
 
-  public ngOnInit(): void {
-      this.win = remote.getCurrentWindow();
-      this.router.events.subscribe((event: RouterEvent) => {
-          if (event.url && event.url.endsWith("viewer")) {
-              this.canNavigate = false;
-          }
-      });
-  }
-  public ngAfterContentChecked(): void {
-      this.showLoaded = this.showService.showLoaded;
-  }
-  public toggleSettingsDropdown(e: Event): void {
-      e.stopPropagation();
-      e.preventDefault();
-      this.showSettingsDropdown = !this.showSettingsDropdown;
-  }
+    public ngOnInit(): void {
+        this.win = remote.getCurrentWindow();
+        this.router.events.subscribe((event: RouterEvent) => {
+            if (event.url && event.url.endsWith("viewer")) {
+                this.canNavigate = false;
+            }
+        });
+    }
+    public ngAfterContentChecked(): void {
+        this.showLoaded = this.showService.showLoaded;
+    }
+    public toggleSettingsDropdown(e: Event): void {
+        e.stopPropagation();
+        e.preventDefault();
+        this.showSettingsDropdown = !this.showSettingsDropdown;
+    }
 
-  public minWindow(): void {
-      this.win.minimize();
-  }
-  public maxWindow(): void {
-      if (this.win.isMaximized()) {
-          this.win.unmaximize();
-          this.isMaximized = false;
-      } else {
-          this.win.maximize();
-          this.isMaximized = true;
-      }
-  }
-  public closeWindow(): void {
-      this.win.close();
-  }
+    public minWindow(): void {
+        this.win.minimize();
+    }
+    public maxWindow(): void {
+        if (this.win.isMaximized()) {
+            this.win.unmaximize();
+            this.isMaximized = false;
+        } else {
+            this.win.maximize();
+            this.isMaximized = true;
+        }
+    }
+    public async closeWindow(): Promise<void> {
+        smalltalk.confirm("Close AGLight", "Do you really want to close the application?").then(() => {
+            this.win.close();
+            remote.app.quit();
+        }, () => undefined);
+    }
 }
