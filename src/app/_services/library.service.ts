1ce801ced253b3f8488930737d47d9f78106f91a
H. Rüger <hannesrueger@gmx.de>
2020-07-24 10:31:08 +0200

    fixed capabilities not an array

--- src/app/_services/library.service.ts
+++ src/app/_services/library.service.ts
@@ -58,7 +58,7 @@ export class LibraryService {
                 if (p.templateChannels) {
                     for (const key of Object.keys(p.templateChannels) as any) {
                         if ((p.templateChannels[key] as any).capability) {
-                            p.templateChannels[key].capabilities = (p.templateChannels[key] as any).capability;
+                            p.templateChannels[key].capabilities = [(p.templateChannels[key] as any).capability];
                             p.templateChannels[key].singleCapability = true;
                         }
                     }
