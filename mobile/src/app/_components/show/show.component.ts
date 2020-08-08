53b47ce399fa36c5620bc2dc75a739413928df8a
Hannes Rüger <hannesrueger@gmx.de>
2020-08-08 11:22:56 +0200

    (mobile) display widget grid

--- mobile/src/app/_components/show/show.component.ts
+++ mobile/src/app/_components/show/show.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import * as dialogs from "tns-core-modules/ui/dialogs";
 import { ConnectionService } from "../../_services/connection.service";
 
 @Component({
@@ -6,11 +7,30 @@ import { ConnectionService } from "../../_services/connection.service";
     templateUrl: "./show.component.html",
 })
 export class ShowComponent {
+    public widgets: any[] = [];
     constructor(private connectionService: ConnectionService) { }
 
     public send(): void {
         this.connectionService.get("widgets").subscribe((data) => {
-            console.log("data", data);
+            if (data && data.widgets) {
+                this.widgets = data.widgets;
+            }
+        }, (error) => {
+            if (error && error.error && error.error.error) {
+                dialogs.alert(error.error.error);
+            } else {
+                dialogs.alert("Unknown error occured");
+            }
         });
+    }
+
+    public getRows(): string {
+        const m = this.widgets.reduce((p, c) => (p.y + p.rows - 1 > c.y + c.rows - 1 ? p : c));
+        return Array(m.y + m.rows).fill("*").join(", ");
+    }
+
+    public getColumns(): string {
+        const m = this.widgets.reduce((p, c) => (p.x + p.cols - 1 > c.x + c.cols - 1 ? p : c));
+        return Array(m.x + m.cols).fill("*").join(", ");
     }
 }
