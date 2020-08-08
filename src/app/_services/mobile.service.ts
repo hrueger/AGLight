01ef629e7bc1c75a86dad7263b41687f7c5d3200
Hannes Rüger <hannesrueger@gmx.de>
2020-08-08 11:23:06 +0200

    (api) serve widgets

--- src/app/_services/mobile.service.ts
+++ src/app/_services/mobile.service.ts
@@ -3,13 +3,25 @@ import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 import * as express from "express";
 import { StatusbarService } from "./statusbar.service";
 import { QRCodeComponent } from "../_components/qrcode/qrcode.component";
+import { ShowService } from "./show.service";
+import { WidgetGridComponent } from "../_components/widget-grid/widget-grid.component";
+import { LibraryService } from "./library.service";
+import { DialogService } from "./dialog.service";
+import { DmxService } from "./dmx.service";
 
 @Injectable({
     providedIn: "root",
 })
 export class MobileService {
     private server: express.Express;
-    constructor(private statusbarService: StatusbarService, private modalService: NgbModal) { }
+    constructor(
+        private statusbarService: StatusbarService,
+        private modalService: NgbModal,
+        private showService: ShowService,
+        private libraryService: LibraryService,
+        private dialogService: DialogService,
+        private dmxService: DmxService,
+    ) { }
 
     public init(): void {
         this.statusbarService.setItem({
@@ -34,6 +46,21 @@ export class MobileService {
             const r = express.Router();
             r.get("/testConnection", (req, res) => {
                 res.send({ success: true });
+            });
+            r.get("/widgets", async (req, res) => {
+                if (!this.showService.showLoaded) {
+                    res.status(500).send({ error: "No show loaded!" });
+                }
+                const wgc = new WidgetGridComponent(
+                    this.showService,
+                    this.libraryService,
+                    this.dmxService,
+                    this.modalService,
+                    this.dialogService,
+                );
+                wgc.editMode = true;
+                await wgc.ngOnInit();
+                res.send({ widgets: wgc.widgets });
             });
             this.server.use(r);
             this.server.listen(4573, () => {
