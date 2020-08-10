860704c7ac5834127cb14551ce3a7f8c6d544280
Hannes Rüger <hannesrueger@gmx.de>
2020-08-10 09:46:51 +0200

    statusbar now supports html

--- src/app/_components/statusbar/statusbar.component.ts
+++ src/app/_components/statusbar/statusbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, ChangeDetectorRef } from "@angular/core";
+import { SafeHtml, DomSanitizer } from "@angular/platform-browser";
 import { StatusbarService } from "../../_services/statusbar.service";
 import { LibraryService } from "../../_services/library.service";
 import { DmxService } from "../../_services/dmx.service";
@@ -20,7 +21,8 @@ export class StatusbarComponent {
     private libraryService: LibraryService,
     private dmxService: DmxService,
     private mobileService: MobileService,
-    private consoleService: ConsoleService) {}
+      private consoleService: ConsoleService,
+      protected sanitizer: DomSanitizer) { }
 
   public ngOnInit(): void {
       this.statusbarService.data.subscribe((d) => {
@@ -50,5 +52,9 @@ export class StatusbarComponent {
       }
       this.cdr.detectChanges();
       this[btn.service][btn.action]();
+  }
+
+  public secure(value: string): SafeHtml {
+      return this.sanitizer.bypassSecurityTrustHtml(value);
   }
 }
