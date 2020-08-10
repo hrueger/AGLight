21b7f224054f9b48959eb86b72e03bcebb9bfaa7
Hannes Rüger <hannesrueger@gmx.de>
2020-08-10 09:46:43 +0200

    (mobile) improved connection process

--- mobile/src/app/_services/connection.service.ts
+++ mobile/src/app/_services/connection.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { Device } from "@nativescript/core";
 
 @Injectable({ providedIn: "root" })
 export class ConnectionService {
@@ -11,7 +12,19 @@ export class ConnectionService {
     public async connect(ip: string): Promise<void> {
         return new Promise<void>((resolve, reject) => {
             this.apiUrl = `http://${ip}:4573/`;
-            this.httpClient.get(`${this.apiUrl}testConnection`).subscribe((d: any) => {
+            this.httpClient.post(`${this.apiUrl}connect`, {
+                device: {
+                    model: Device.model,
+                    deviceType: Device.deviceType,
+                    os: Device.os,
+                    osVersion: Device.osVersion,
+                    sdkVersion: Device.sdkVersion,
+                    language: Device.language,
+                    manufacturer: Device.manufacturer,
+                    uuid: Device.uuid,
+                    region: Device.region,
+                },
+            }).subscribe((d: any) => {
                 if (d && d.success === true) {
                     this._isConnected = true;
                     resolve();
@@ -23,6 +36,14 @@ export class ConnectionService {
     }
     public get(url: string): Observable<any> {
         return this.httpClient.get(`${this.apiUrl}${url}`);
+    }
+    public post(url: string, data: Record<string, unknown>): Observable<any> {
+        return this.httpClient.post(`${this.apiUrl}${url}`, data);
+    }
+    public disconnect(): void {
+        this.post("disconnect", { deviceId: Device.uuid }).subscribe(() => {
+            this._isConnected = false;
+        });
     }
 
     public get isConnected(): boolean {
