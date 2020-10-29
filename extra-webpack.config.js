5740291564f2debcbd55a8d2f87a2c070c8081ab
Hannes Rüger <hannesrueger@gmx.de>
2020-10-29 17:43:55 +0100

    removed `sqlite3` + `typeorm` from webpack config

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -3,18 +3,11 @@ module.exports = {
     // don't remove this comments it is used for parsing by postinstall.config.js
     // start of extra configs
     externals: {
-        typeorm: "require('typeorm')",
-        sqlite3: "require('sqlite3')",
         dmx: "require('dmx')",
         serialport: "require('serialport')",
         ws: "require('ws')",
         "socket.io": "require('socket.io')",
         "express": "require('express')",
     },
-    resolve: {
-        alias: {
-            typeorm: "__dirname../node_modules/typeorm/typeorm-model-shim"
-        }
-    }
     // end of extra configs
 };
