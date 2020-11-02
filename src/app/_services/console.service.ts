ebc267e5f592a298cfc89b959a87d0039fe117ce
Hannes Rüger <hannesrueger@gmx.de>
2020-11-02 10:16:14 +0100

    automatically reconnect to console

--- src/app/_services/console.service.ts
+++ src/app/_services/console.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from "@angular/core";
-import { hub, Tile as TileType, TileEncoder12, TileEncoder8, TileFader4, TileLedButton12, TileLedButton8 } from " ../../../../makehaus-js/dist";
+import {
+    hub, Tile as TileType, TileEncoder12, TileEncoder8, TileFader4, TileLedButton12, TileLedButton8,
+} from " ../../../../makehaus-js/dist";
 import { StatusbarService } from "./statusbar.service";
 import { DialogService } from "./dialog.service";
+import { SettingsService } from "./settings.service";
 
 type Tile = TileLedButton8 | TileLedButton12 | TileEncoder8 | TileEncoder12 | TileFader4;
+enum Settings {
+    CONSOLE_IP = "CONSOLE_IP",
+}
 
 @Injectable({
     providedIn: "root",
@@ -14,9 +20,17 @@ export class ConsoleService {
     constructor(
         private statusbarService: StatusbarService,
         private dialogService: DialogService,
+        private settingsService: SettingsService,
     ) { }
 
     public init(): void {
+        this.setStatusbarNotConnected();
+        const lastIP = this.settingsService.get(Settings.CONSOLE_IP);
+        if (lastIP) {
+            this.connectToHub(lastIP, false);
+        }
+    }
+    private setStatusbarNotConnected() {
         this.statusbarService.setItem({
             name: "No console connected",
             icon: "times",
@@ -35,7 +49,16 @@ export class ConsoleService {
             },
         });
     }
-    public async connect(): Promise<void> {
+
+    public async connect(userFeedback = true): Promise<void> {
+        this.dialogService.prompt("Connect to console", "Input the IP Address of your console.", this.settingsService.get(Settings.CONSOLE_IP) || "").then((ip: string) => {
+            this.connectToHub(ip, userFeedback);
+        }, () => {
+            this.setStatusbarNotConnected();
+        });
+    }
+
+    private connectToHub(ip: string, userFeedback: boolean) {
         this.statusbarService.setItem({
             name: "Connecting to console",
             icon: "spinner fa-spin",
@@ -45,34 +68,37 @@ export class ConsoleService {
                 content: "Please wait...",
             },
         });
-        this.dialogService.prompt("Connect to console", "Input the IP Address of your console.", "192.168.178.85").then((ip: string) => {
-            hub.init(ip, 8192).then(() => {
-                hub.on(TileType.LEDBUTTON12, (t) => this.tiles.push(t));
-                hub.on(TileType.LEDBUTTON8, (t) => this.tiles.push(t));
-                hub.on(TileType.MOTORFADER4, (t) => this.tiles.push(t));
-                hub.on(TileType.ENCODER8, (t) => this.tiles.push(t));
-                hub.on(TileType.ENCODER12, (t) => this.tiles.push(t));
+        hub.init(ip, 8192).then(() => {
+            hub.on(TileType.LEDBUTTON12, (t) => this.tiles.push(t));
+            hub.on(TileType.LEDBUTTON8, (t) => this.tiles.push(t));
+            hub.on(TileType.MOTORFADER4, (t) => this.tiles.push(t));
+            hub.on(TileType.ENCODER8, (t) => this.tiles.push(t));
+            hub.on(TileType.ENCODER12, (t) => this.tiles.push(t));
 
-                setTimeout(() => {
-                    if (this.tiles.length > 0) {
-                        this.statusbarService.setItem({
-                            name: "MakeHaus",
-                            icon: "home",
-                            id: "console",
-                            dropup: {
-                                title: "Connected",
-                                content: "",
-                            },
-                        });
-                    } else {
-                        this.showError("No tiles found.");
-                    }
-                }, 1000);
-            }, (e) => {
+            setTimeout(() => {
+                if (this.tiles.length > 0) {
+                    this.statusbarService.setItem({
+                        name: "MakeHaus",
+                        icon: "home",
+                        id: "console",
+                        dropup: {
+                            title: "Connected",
+                            content: "",
+                        },
+                    });
+                    this.settingsService.set(Settings.CONSOLE_IP, ip);
+                } else if (userFeedback) {
+                    this.showError("No tiles found.");
+                } else {
+                    this.setStatusbarNotConnected();
+                }
+            }, 1000);
+        }, (e) => {
+            if (userFeedback) {
                 this.showError(e);
-            });
-        }, () => {
-            this.init();
+            } else {
+                this.setStatusbarNotConnected();
+            }
         });
     }
 
